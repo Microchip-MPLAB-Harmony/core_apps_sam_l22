310b873c024b2e41bf17417b33ca207cb4950116
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-17 16:30:48 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_instance_sam_l22_xpro
    MH3-56653
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
+++ apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l22_xpro/peripheral/port/plib_port.c
@@ -75,8 +75,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_DIR = 0x120000;
    PORT_REGS->GROUP[0].PORT_OUT = 0x120000;
    PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[20] = 0x0;
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
@@ -87,6 +89,9 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PINCFG[0] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[1] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[2] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[4] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PINCFG[8] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0;
 
    PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x22;
    PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x2;
@@ -96,6 +101,8 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[2].PORT_DIR = 0x18000000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x18000002;
    PORT_REGS->GROUP[2].PORT_PINCFG[1] = 0x6;
+   PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PINCFG[28] = 0x0;
 
 
 }
@@ -309,7 +316,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -320,7 +327,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
