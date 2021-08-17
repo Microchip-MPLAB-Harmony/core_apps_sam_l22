6b227fa5e28cb6c927213d696233a3c3a5fe4012
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-17 16:36:33 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system_console_debug_sys_console_debug_uart_read_write_sam_l22_xpro
    MH3-56658
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/sam_l22_xpro/initialization.c
+++ apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/sam_l22_xpro/initialization.c
@@ -163,7 +163,7 @@ const SYS_DEBUG_INIT debugInit =
 void SYS_Initialize ( void* data )
 {
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3UL);
 
     PM_Initialize();
 
