3603d9d6e68c031f81668e6153a331e9555a7005
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-05-09 14:51:52 +0530

    [SYS_CONSOLE] Regenerated demo for system_console_debug_sys_console_debug_usb_read_write_sam_l22_xpro
    MH3-76970

--- apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/sam_l22_xpro/definitions.h
@@ -50,12 +50,12 @@
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
 #include "driver/usb/usbfsv1/drv_usbfsv1.h"
@@ -76,6 +76,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
@@ -192,7 +198,7 @@ typedef struct
 {
     SYS_MODULE_OBJ  sysConsole1;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
     SYS_MODULE_OBJ  sysDebug;
 
