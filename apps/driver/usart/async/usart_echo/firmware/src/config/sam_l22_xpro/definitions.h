66a5cbba1f5dc980de62c785cba325fd3ea1f978
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-05-09 14:51:49 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_echo_sam_l22_xpro
    MH3-76965

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_l22_xpro/definitions.h
@@ -51,10 +51,10 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "driver/usart/drv_usart.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "bsp/bsp.h"
 #include "system/int/sys_int.h"
@@ -72,6 +72,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML22N18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML22"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 32000000
