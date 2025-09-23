bb1864ba1dfec961164ee55559022aff4dc9cdd4
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:10:57 +0000

    [APP] Regenerated demo for system__console_debug__sys_console_debug_usb_read_write__firmware
    MH3C-350

--- apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/sam_l22_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -53,9 +53,9 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
-#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
 #include "driver/usb/usbfsv1/drv_usbfsv1.h"
