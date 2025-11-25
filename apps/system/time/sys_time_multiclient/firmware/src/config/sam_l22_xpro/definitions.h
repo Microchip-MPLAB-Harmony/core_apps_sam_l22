713f3c59eb5d9a1538ea8c43edf7b3350f8c0f85
Syam Nair <Syam.Nair@microchip.com>
2025-11-25 11:01:54 +0000

    Regen all applications to fix component yamls
    MH3C-397

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_l22_xpro/definitions.h
@@ -58,6 +58,7 @@
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
